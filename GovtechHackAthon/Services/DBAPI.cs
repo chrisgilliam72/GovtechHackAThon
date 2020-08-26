e2d82c75acc0951543ea5726398f5e2390edfc48
chrisgilliam72 <54978118+chrisgilliam72@users.noreply.github.com>
2020-08-26 14:21:58 +0200

    Add files via upload

---
+++ GovtechHackAthon/Services/DBAPI.cs
@@ -0,0 +1,71 @@
+﻿using GovtechDBLib.Models;
+using GovtechHackAthon.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GovtechHackAthon.Services
+{
+    public class DBAPI
+    {
+        private readonly GovtechHackathonContext govtechHackathonContext;
+
+        public DBAPI(GovtechHackathonContext govtechHackathonContext)
+        {
+            this.govtechHackathonContext = govtechHackathonContext;
+        }
+
+        public IEnumerable<UserDetails> GetUserList()
+        {
+            var userList = new UserList();
+            var ctx = govtechHackathonContext;
+            var dbUser = ctx.User.Include("FkProvince").Include("FkUserLevel").ToList();
+            return dbUser.Select(x => (UserDetails)x).ToList();
+        }
+
+        public async Task<IEnumerable<UserDetails>> GetUserListAsync()
+        {
+           
+            var ctx = govtechHackathonContext;
+            var dbUser = await ctx.User.Include("FkProvince").Include("FkUserLevel").ToListAsync();
+            return dbUser.Select(x => (UserDetails)x).ToList();
+        }
+
+        public IEnumerable<UserDetails> GetAllUsers()
+        {
+            var ctx = govtechHackathonContext;
+            var dbUsers = ctx.User.Where(x => x.IsActive).ToList();
+            var lstUsers = dbUsers.Select(x => (UserDetails)x).ToList();
+            return lstUsers;
+        }
+
+        public IEnumerable<UserDetails> GetUnnassignedUsers()
+        {
+            var ctx = govtechHackathonContext;
+            var dbUsers = ctx.User.Include("UsersInGroup").Where(x => x.IsActive && x.UsersInGroup.Count==0).ToList();
+            var lstUsers = dbUsers.Select(x => (UserDetails)x).ToList();
+            return lstUsers;
+        }
+
+        public IEnumerable<GroupItem> GetAllGroups()
+        {
+            var ctx = govtechHackathonContext;
+            var dbUsers = ctx.Group.Where(x => x.Active).ToList();
+            var lstGrps = dbUsers.Select(x => (GroupItem)x).ToList();
+            return lstGrps;
+        }
+
+
+       public IEnumerable<TeamMemberData> GetTeamMembers()
+        {
+            var ctx = govtechHackathonContext;
+            var dbTeamList = ctx.TeamMember.Include("FkRace").
+                                Include("FkGender").
+                                Include("FkCountry").ToList();
+            var list = dbTeamList.Select(x => (TeamMemberData)x).ToList();
+            return list;
+        }
+    }
+}
---
+++ GovtechHackAthon/Services/DBLookupService.cs
@@ -0,0 +1,123 @@
+﻿using GovtechDBLib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GovtechHackAthon.Services
+{
+    public class DBLookupService
+    {
+        private readonly GovtechHackathonContext govtechHackathonContext;
+
+        public DBLookupService(GovtechHackathonContext govtechHackathonContext)
+        {
+            this.govtechHackathonContext = govtechHackathonContext;
+        }
+
+        public IEnumerable<CaseCategory> GetCategories()
+        {
+            var ctx = govtechHackathonContext;
+            return ctx.CaseCategory.OrderBy(x => x.Name).ToList();
+        }
+        public IEnumerable<ScoringCategories> GetScoringCategories()
+        {
+            var ctx = govtechHackathonContext;
+            return ctx.ScoringCategories.Include("FkType").OrderBy(x=>x.PkId).ToList();
+        }
+
+
+        public IEnumerable<InvestorIndustry> GetInvestorIndustries()
+        {
+            var ctx = govtechHackathonContext;
+            return ctx.InvestorIndustry.OrderBy(x => x.Description);
+        }
+
+        public IEnumerable<Genders> GetGenders()
+        {
+            var ctx = govtechHackathonContext;
+            using (ctx)
+            {
+                return ctx.Genders.ToList();
+            }
+
+        }
+
+        public IEnumerable<Provinces> GetProvinces()
+        {
+            var ctx = govtechHackathonContext;
+            using (ctx)
+            {
+                var provinces = ctx.Provinces.OrderBy(x => x.Name).ToList();
+                var NAProvince = provinces.FirstOrDefault(x => x.Name == "N/A");
+                if (NAProvince!=null)
+                {
+                    provinces.Remove(NAProvince);
+                    provinces.Add(NAProvince);
+                }
+
+                return provinces;
+            }
+        }
+
+        public IEnumerable<Race> GetRaces()
+        {
+            var ctx = govtechHackathonContext;
+            using (ctx)
+            {
+                return ctx.Race.OrderBy(x => x.Name).ToList();
+            }
+        }
+
+        public IEnumerable<SituationOptions> GetSituationOptions()
+        {
+            var ctx = govtechHackathonContext;
+            using (ctx)
+            {
+                return ctx.SituationOptions.ToList();
+            }
+        }
+
+        public IEnumerable<SituationStatements> GetSituationStatements()
+        {
+            var ctx =  govtechHackathonContext;
+            using (ctx)
+            {
+                return ctx.SituationStatements.ToList();
+            }
+        }
+
+        public IEnumerable<Outcomes> GetOutcomes()
+        {
+            var ctx = govtechHackathonContext;
+            using (ctx)
+            {
+                return ctx.Outcomes.ToList();
+            }
+        }
+
+        public IEnumerable<Languages> GetLanguages()
+        {
+            var ctx = govtechHackathonContext;
+            using (ctx)
+            {
+                return ctx.Languages.OrderBy(x=>x.Language).ToList();
+            }
+        }
+
+        public IEnumerable<Countries> GetCountries()
+        {
+            var ctx = govtechHackathonContext;
+            using (ctx)
+            {
+                return ctx.Countries.OrderBy(x => x.Name).ToList();
+            }
+        }
+
+        public IEnumerable<UserLevel> GetUserLevels()
+        {
+            return govtechHackathonContext.UserLevel.ToList(); ;
+        }
+    }
+}
